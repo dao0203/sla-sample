93e9aa5f679e7080de8b64e5c2286a5f2d72c5d9
hirotask <hirotaskhashi@gmail.com>
2023-11-21 12:17:47 +0900

    [update]: -eオプションを削除

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 docker-compose up --build -d
 docker-compose exec myclang gcc main.c -o main
-echo -e "\n--- Result is Here ---"
+echo "\n--- Result is Here ---"
 docker-compose exec myclang /bin/bash -c "./main"
